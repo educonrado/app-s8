77d79c0c4d53b71ce08966f297798285f03b543a
Conrado Zapata <excz010715@sri.ad>
2025-04-30 08:04:53 -0500

    Estructura inicial

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace app_s8
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
